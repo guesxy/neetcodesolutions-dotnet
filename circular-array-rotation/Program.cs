f152aa0580c19484741336e8a5f999a254e02187
Vitaly K <guesxy@gmail.com>
2025-03-14 09:38:29 +0000

    Initial commit. 2 solutions

---
+++ circular-array-rotation/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+    * Complete the 'circularArrayRotation' function below.
+    *
+    * The function is expected to return an INTEGER_ARRAY.
+    * The function accepts following parameters:
+    *  1. INTEGER_ARRAY a
+    *  2. INTEGER k
+    *  3. INTEGER_ARRAY queries
+*/
+
+int[] a = {3,4,5,6};
+        
+Console.WriteLine("Start");
+        
+int[] result = TwoSum(a, 7);
+
+foreach (var res in result)
+    Console.WriteLine(res);
+
+public static int[] TwoSum(int[] nums, int target) {
+    var map = new Dictionary<int, int>();
+    var results = new List<int>();
+
+    for (var i = 0; i < nums.Length; i++)
+    {
+        if (!map.ContainsKey(nums[i]))
+        {
+            map.Add(nums[i], i);
+        }
+    }
+
+    for (var j = 0; j < nums.Length; j++)
+    {
+        int complement = target - nums[j];
+    
+        if (map.ContainsKey(complement) && map[complement] != j && !results.Contains(j)) {
+            results.Add(map[complement]);
+            results.Add(j);
+        }
+    }
+    
+    int[] resultArray = results.ToArray();
+    Array.Sort(resultArray);
+    
+    return resultArray;
+}
---
+++ string-array-group-anagrams/Program.cs
@@ -0,0 +1,28 @@
+﻿//Input: strs = ["act","pots","tops","cat","stop","hat"]
+//Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+
+string[] strings = ["act", "pots", "tops", "cat", "stop", "hat"];
+var results = GroupAnagrams(strings);
+
+foreach (var r in results)
+    Console.WriteLine(r);
+
+static List<List<string>> GroupAnagrams(string[] strs)
+{
+    var anagramMap = new Dictionary<string, List<string>>();
+
+    foreach (var str in strs)
+    {
+        var charArray = str.ToCharArray();
+        Array.Sort(charArray);
+        var sortedStr = new string(charArray);
+
+        if (!anagramMap.ContainsKey(sortedStr))
+        {
+            anagramMap[sortedStr] = new List<string>();
+        }
+        anagramMap[sortedStr].Add(str);
+    }
+
+    return anagramMap.Values.ToList();
+}

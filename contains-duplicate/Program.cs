04af7c6c3449bc141b028ba57b6143c231f31861
Vitaly K <guesxy@gmail.com>
2025-03-15 00:28:45 +0000

    Solution for sequence equation and duplicate finder

---
+++ contains-duplicate/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] nums = [1, 2, 3, 3];
+
+bool hasDupes = false;
+
+for (var i = 0; i < nums.Length; i++)
+{
+    for (var j = i+1; j < nums.Length; j++)
+    {
+        if (nums[i] == nums[j])
+        {
+            hasDupes = true;
+            break;
+        }
+    }
+}
+
+Console.WriteLine(hasDupes);
---
+++ sequence-equation/Program.cs
@@ -0,0 +1,15 @@
+﻿var p = new List<int> { 5, 2, 1, 3, 4 }; //{ 2, 3, 1 };
+var map = new Dictionary<int, int>();
+var results = new List<int>();
+
+for (var i = 0; i < p.Count(); i++)
+{
+    map.Add(p[i], i + 1);
+}
+
+for (var i = 0; i < p.Count(); i++)
+{
+    Console.WriteLine(map[map[i + 1]]); 
+    //for 231 case, 1 -> map[1] = 3 -> map[map[1]] -> map[3] -> 2
+    results.Add(map[map[i + 1]]);
+}
